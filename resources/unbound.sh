3399f56acbae9c649abf1bdb7c160decceb14795
JackGruber <24863925+JackGruber@users.noreply.github.com>
2018-05-12 15:44:26 +0200

    Option DISABLE_CONF_VARS

--- resources/unbound.sh
+++ resources/unbound.sh
@@ -19,35 +19,38 @@ USE_CAPS_FOR_ID=${USE_CAPS_FOR_ID:-yes}
 USE_LOGFILE=${ENABLE_LOGFILE:-no}
 USE_CHROOT=${ENABLE_LOGFILE:-yes}
 ENABLE_REMOTE_CONTROL=${ENABLE_REMOTE_CONTROL:-no}
+DISABLE_CONF_VARS=${DISABLE_CONF_VARS:-no}
 
-sed 's/{{PORT}}/'"${PORT}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{DO_IPV6}}/'"${DO_IPV6}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{DO_IPV4}}/'"${DO_IPV4}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{DO_UDP}}/'"${DO_UDP}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{DO_TCP}}/'"${DO_TCP}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{INTERFACE}}/'"${INTERFACE}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{SO_REUSEPORT}}/'"${SO_REUSEPORT}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{HIDE_IDENTITY}}/'"${HIDE_IDENTITY}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{HIDE_VERSION}}/'"${HIDE_VERSION}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{VERBOSITY}}/'"${VERBOSITY}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{NUM_THREADS}}/'"${NUM_THREADS}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{QNAME_MINIMISATION}}/'"${QNAME_MINIMISATION}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{RRSET_ROUNDROBIN}}/'"${RRSET_ROUNDROBIN}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{USE_CAPS_FOR_ID}}/'"${USE_CAPS_FOR_ID}"'/' -i /opt/unbound/etc/unbound/unbound.conf
-sed 's/{{ENABLE_REMOTE_CONTROL}}/'"${ENABLE_REMOTE_CONTROL}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+if [ "x${DISABLE_CONF_VARS}" = "xno" ]; then
+  sed 's/{{PORT}}/'"${PORT}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{DO_IPV6}}/'"${DO_IPV6}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{DO_IPV4}}/'"${DO_IPV4}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{DO_UDP}}/'"${DO_UDP}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{DO_TCP}}/'"${DO_TCP}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{INTERFACE}}/'"${INTERFACE}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{SO_REUSEPORT}}/'"${SO_REUSEPORT}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{HIDE_IDENTITY}}/'"${HIDE_IDENTITY}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{HIDE_VERSION}}/'"${HIDE_VERSION}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{VERBOSITY}}/'"${VERBOSITY}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{NUM_THREADS}}/'"${NUM_THREADS}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{QNAME_MINIMISATION}}/'"${QNAME_MINIMISATION}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{RRSET_ROUNDROBIN}}/'"${RRSET_ROUNDROBIN}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{USE_CAPS_FOR_ID}}/'"${USE_CAPS_FOR_ID}"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  sed 's/{{ENABLE_REMOTE_CONTROL}}/'"${ENABLE_REMOTE_CONTROL}"'/' -i /opt/unbound/etc/unbound/unbound.conf
 
-if [ "x${ENABLE_REMOTE_CONTROL}" = "xyes" ]; then
-  if [ ! -f /opt/unbound/etc/unbound/unbound_control.key ]; then
-    /opt/unbound/sbin/unbound-control-setup 1>/dev/null 2>&1 || true
+  if [ "x${ENABLE_REMOTE_CONTROL}" = "xyes" ]; then
+    if [ ! -f /opt/unbound/etc/unbound/unbound_control.key ]; then
+      /opt/unbound/sbin/unbound-control-setup 1>/dev/null 2>&1 || true
+    fi
   fi
-fi
 
-if [ "x${USE_LOGFILE}" = "xyes" ]; then
-  mkdir -p /opt/unbound/etc/unbound/log && \
-     chown unbound.unbound /opt/unbound/etc/unbound/log
-  sed 's/{{LOGFILE}}/'"log/unbound.log"'/' -i /opt/unbound/etc/unbound/unbound.conf
-else
-  sed 's/{{LOGFILE}}/'""'/' -i /opt/unbound/etc/unbound/unbound.conf
+  if [ "x${USE_LOGFILE}" = "xyes" ]; then
+    mkdir -p /opt/unbound/etc/unbound/log && \
+       chown unbound.unbound /opt/unbound/etc/unbound/log
+    sed 's/{{LOGFILE}}/'"log/unbound.log"'/' -i /opt/unbound/etc/unbound/unbound.conf
+  else
+    sed 's/{{LOGFILE}}/'""'/' -i /opt/unbound/etc/unbound/unbound.conf
+  fi
 fi
 
 if [ "x${USE_CHROOT}" = "xno" ]; then
